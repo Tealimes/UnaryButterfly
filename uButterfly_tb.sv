10ed4987b4ca20e6d3e1cfe12b4c33eaaab4b41f
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-07-07 13:23:41 -0400

    added class and fixed some issues

--- uButterfly_tb.sv
+++ uButterfly_tb.sv
@@ -9,8 +9,14 @@
 
 //used to check errors
 class errorcheck;
-    real uResult;
-    real eResult;
+    real uResult1;
+    real uResult2;
+    real uResult3;
+    real uResult4;
+    real eResult1;
+    real eResult2;
+    real eResult3;
+    real eResult4;
     real fnum;
     real fdenom;
     real cntReal0;
@@ -19,6 +25,10 @@ class errorcheck;
     real cntImg1;
     real cntiwImg;
     real cntiwReal;
+    real outReal0;
+    real outImg0;
+    real outReal1;
+    real outImg1;
     real asum;
     real mse;
     real rmse;
@@ -34,44 +44,105 @@ class errorcheck;
         cntImg1 = 0;
         cntiwReal = 0;
         cntiwImg = 0;
+        outReal0 = 0;
+        outImg0 = 0;
+        outReal1 = 0;
+        outReal0 = 0;
         j = 0;
     endfunction
 
     //accumulates to account for bitstreams
-    function count(real a, b, c, d, e, f);
+    function count(real a, b, c, d, e, f, oA, oB, oC, oD);
         cntReal0 = cntReal0 + a;
         cntImg0 = cntImg0 + b;
         cntReal1 = cntReal1 + c;
         cntImg1 = cntImg1 + d;
         cntiwReal = cntiwReal + e;
         cntiwImg = cntiwImg + f;
+        outReal0 = outReal0 + oA;
+        outImg0 = outImg0 + oB;
+        outReal1 = outReal1 + oC;
+        outImg1 = outImg1 + oD;
+
         fdenom++;
     endfunction
 
     //sums the results of a bitstream cycle
     function fSUM();
+        real biReal0; 
+        real biImg0;
+        real biReal1;
+        real biImg1;
+        real biwReal;
+        real biwImg;
 
         j++; //counts current run
+
+
+        biReal0 = (2*(cntReal0/fdenom)) - 1;
+        biImg0 = (2*(cntImg0/fdenom)) - 1;
+        biReal1 = (2*(cntReal1/fdenom)) - 1;
+        biImg1 = (2*(cntImg1/fdenom)) - 1;
+        biwReal = (2*(cntiwReal/fdenom)) - 1;
+        biwImg = (2*(cntiwImg/fdenom)) - 1;
 
         //bipolar representation
         
         $display("Run <%.0f>: ", j);
         $display("Length of bitstream = %.0f", fdenom);
-        $display("Number of 1s in output = %.0f", fnum);
+        $display("Number of 1s in input Real0 = %.0f", cntReal0);
+        $display("Number of 1s in input Img0 = %.0f", cntImg0);
+        $display("Number of 1s in input Real1 = %.0f", cntReal1);
+        $display("Number of 1s in input Img1 = %.0f", cntImg1);
+        $display("Number of 1s in input wReal = %.0f", cntiwReal);
+        $display("Number of 1s in input wImg = %.0f", cntiwImg);
+        $display("Number of 1s in output Real0 = %.0f", outReal0);
+        $display("Number of 1s in output Img0 = %.0f", outImg0);
+        $display("Number of 1s in output Real1 = %.0f", outReal1);
+        $display("Number of 1s in output Img1 = %.0f", outImg1);
 
-        //uResult = ; //unary result
-        //eResult = ; //expected result
+        $display("Bipolar Real0 value = %.9f", biReal0);
+        $display("Bipolar Image0 value = %.9f", biImg0);
+        $display("Bipolar Real1 value = %.9f", biReal1);
+        $display("Bipolar Image1 value = %.9f", biImg0);
+        $display("Bipolar wReal value = %.9f", biwReal);
+        $display("Bipolar wImg value = %.9f", biwImg);
 
-        $display("Unary result = %.9f", uResult);
-        $display("Expected result = %.9f", eResult); 
+        //unary result
+        uResult1 = (2*(outReal0/fdenom)) - 1;
+        uResult2 = (2*(outImg0/fdenom)) - 1;
+        uResult3 = (2*(outReal1/fdenom)) - 1;
+        uResult4 = (2*(outImg1/fdenom)) - 1;
 
-        asum = asum + ((uResult - eResult) * (uResult - eResult));
-        $display("Cumulated square error = %.9f", asum);
-        $display("");
+        //expected results
+        eResult1 = (biReal0 + ((biReal1*biwReal) - (biImg1*biwImg)))/4; 
+        eResult2 = (biImg0 + ((biReal1*biwImg) + (biImg1*biwReal)))/4;
+        eResult3 = (biReal0 - ((biReal1*biwReal) - (biImg1*biwImg)))/4;
+        eResult4 = (biImg0 - ((biReal1*biwImg) + (biImg1*biwReal)))/4;
+
+        $display("Unary result 0 = %.9f + %.9fi", uResult1, uResult2);
+        $display("Unary result 1 = %.9f + %.9fi\n", uResult3, uResult4);
+        
+        $display("Expected result 0 = %.9f + %.9fi", eResult1, eResult2);
+        $display("Expected result 1 = %.9f + %.9fi\n", eResult3, eResult4);
+
+        //asum = asum + ((uResult - eResult) * (uResult - eResult));
+        //$display("Cumulated square error = %.9f", asum);
+        //$display("");
 
         //resets for next bitstreams
 
         fdenom = 0;
+        cntReal0 = 0;
+        cntImg0 = 0;
+        cntReal1 = 0;
+        cntImg1 = 0;
+        cntiwReal = 0;
+        cntiwImg = 0;
+        outReal0 = 0;
+        outImg0 = 0;
+        outReal1 = 0;
+        outImg1 = 0;
     endfunction
 
     //mean squared error
@@ -104,6 +175,8 @@ module uButterfly_tb();
     logic [BITWIDTH-1:0] iwImg;
     logic iClr;
     logic loadB;
+    logic oBReal;
+    logic oBImg;
     logic oReal0;
     logic oImg0;
     logic oReal1;
@@ -139,6 +212,12 @@ module uButterfly_tb();
     logic result4 [PPCYCLE-1:0];
     logic result_expected4;
     assign result_expected4 = oImg1;
+    logic resultwReal [PPCYCLE-1:0];
+    logic result_expectedwReal;
+    assign result_expectedwReal = oBReal;
+    logic resultwImg [PPCYCLE-1:0];
+    logic result_expectedwImg;
+    assign result_expectedwImg = oBImg;
 
     genvar i;
     generate
@@ -149,11 +228,15 @@ module uButterfly_tb();
                     result2[i] <= 0;
                     result3[i] <= 0;
                     result4[i] <= 0;
+                    resultwReal[i] <= 0;
+                    resultwImg[i] <= 0;
                 end else begin
                     result1[i] <= result1[i-1];
                     result2[i] <= result2[i-1];
                     result3[i] <= result3[i-1];
                     result4[i] <= result4[i-1];
+                    resultwReal[i] <= resultwReal[i-1];
+                    resultwImg[i] <= resultwImg[i-1];
                 end
             end
         end
@@ -165,11 +248,15 @@ module uButterfly_tb();
             result2[0] <= 0;
             result3[0] <= 0;
             result4[0] <= 0;
+            resultwReal[0] <= 0;
+            resultwImg[0] <= 0;
         end else begin
             result1[0] <= result_expected1;
             result2[0] <= result_expected2;
             result3[0] <= result_expected3;
             result4[0] <= result_expected4;
+            resultwReal[0] <= result_expectedwReal;
+            resultwImg[0] <= result_expectedwImg;
         end
     end
     // end here
@@ -232,6 +319,8 @@ module uButterfly_tb();
         .iImg1(iImg1),
         .iwReal(iwReal),
         .iwImg(iwImg),
+        .oBReal(oBReal),
+        .oBImg(oBImg),
         .oReal0(oReal0),
         .oImg0(oImg0),
         .oReal1(oReal1),
@@ -243,7 +332,6 @@ module uButterfly_tb();
     initial begin 
         $dumpfile("uButterfly_tb.vcd"); $dumpvars;
 
-        
         iClk = 1;
         iReal0 = 0;
         iImg0 = 0;
@@ -254,28 +342,35 @@ module uButterfly_tb();
         iwImg = 0;
         rand_a = 0;
         rand_b = 0;
+        rand_c = 0;
+        rand_d = 0;
         iClr = 0;
         loadB = 1;
+        error = new;
 
         #10;
         iRstN = 1;
 
         
         //specified cycles of unary bitstreams
+        repeat(`TESTAMOUNT) begin
+            rand_a = $urandom_range(255);
+            rand_b = $urandom_range(255);
+            rand_c = $urandom_range(255);
+            rand_d = $urandom_range(255);
+            iwReal = $urandom_range(255);
+            iwImg = $urandom_range(255);
 
-        rand_a = $urandom_range(255);
-        rand_b = $urandom_range(255);
-        rand_c = $urandom_range(255);
-        rand_d = $urandom_range(255);
-        iwReal = $urandom_range(255);
-        iwImg = $urandom_range(255);
+            repeat(256) begin
+                #10; 
+                iReal0 = (rand_a > sobolseq_tbA);
+                iImg0 = (rand_b > sobolseq_tbB);
+                iReal1 = (rand_c > sobolseq_tbC);
+                iImg1 = (rand_d > sobolseq_tbD);
+                error.count(iReal0, iImg0, iReal1, iImg1, resultwReal[PPCYCLE-1], resultwImg[PPCYCLE-1], result1[PPCYCLE-1], result2[PPCYCLE-1], result3[PPCYCLE-1], result4[PPCYCLE-1]);
+            end
 
-        repeat(256) begin
-            #10; 
-            iReal0 = (rand_a > sobolseq_tbA);
-            iImg0 = (rand_b > sobolseq_tbB);
-            iReal1 = (rand_c > sobolseq_tbC);
-            iImg1 = (rand_d > sobolseq_tbD);
+            error.fSUM();
         end
 
         iClr = 1;
